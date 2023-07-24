5f814080152b863b5affa8ec89f60e542da77fba
David H <davidweh7878@hotmail.com>
2023-07-24 17:58:26 -0500

    reservations model and router with guests and timestamps

--- models/reservation.js
+++ models/reservation.js
@@ -1,28 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const reservationSchema = new Schema({
-  firstname: {
-    type: String,
-    required: true,
+const reservationSchema = new Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    guests: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+      min: new Date(),
+    },
+    notes: {
+      type: String,
+    },
   },
-  lastname: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    required: true,
-    min: new Date(),
-  },
-  notes: {
-    type: String,
+  {
+    timestamps: true,
   }
-});
+);
 
 const Reservation = mongoose.model('Reservation', reservationSchema);
 
